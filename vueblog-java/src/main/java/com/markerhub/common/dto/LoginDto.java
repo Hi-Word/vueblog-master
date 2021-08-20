c28173a3fccfc237a0773747694a8d62eab69dde
Hi-Word <1054696944@qq.com>
2021-08-20 11:56:55 +0800

    Initial commit

---
+++ vueblog-java/src/main/java/com/markerhub/common/dto/LoginDto.java
@@ -0,0 +1,24 @@
+package com.markerhub.common.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+public class LoginDto implements Serializable {
+
+    @NotBlank(message = "昵称不能为空")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+    public @NotBlank(message = "密码不能为空") String getPassword() {
+        return password;
+    }
+
+    public @NotBlank(message = "昵称不能为空") String getUsername() {
+        return username;
+    }
+}
